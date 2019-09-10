093756c98457122ffaa315a0dd886ff63c6b6ab5
amulran <amulran@amulranL>
2019-09-10 12:58:05 +0530

    Signed-off-by: amulran <amulran@amulranL>

---
+++ Gitfiles/src/Gitpractice/samplegit.java
@@ -0,0 +1,13 @@
+package Gitpractice;
+
+public class samplegit {
+	
+	public static void main(String args[])
+	{
+		System.out.println("This is my first java program");
+		System.out.println("This is my first java program");
+		System.out.println("This is my first java program");
+		System.out.println("This is my first java program");
+	}
+
+}
