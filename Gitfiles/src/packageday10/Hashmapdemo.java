f230ae8a76d920473a482d5e1ca7865cc15ed865
amulran <amulran@amulranL>
2019-09-11 11:07:04 +0530

    added hashmap and arraylist

---
+++ Gitfiles/src/packageday10/Hashmapdemo.java
@@ -0,0 +1,31 @@
+package packageday10;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashmapdemo {
+
+	public static void main(String[] args) {
+		//HashMap hm=new HashMap();
+		
+	HashMap<Integer,String> hm=new HashMap<Integer,String>();
+		
+	hm.put(101,"john");
+	hm.put(102,"scott");
+	hm.put(103,"davis");
+	hm.put(104, "kim");
+	
+	System.out.println(hm);
+	hm.remove(103);
+	System.out.println("after removing elements"+hm);
+	
+	for(Map.Entry m:hm.entrySet())
+	{
+		System.out.println(m.getKey()+" "+m.getValue());
+	}
+		
+	
+	
+		
+	}
+
+}
