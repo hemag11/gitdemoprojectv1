6971e0737c94ad63140134c427cb07610309e020
amulran <amulran@amulranL>
2019-09-14 10:59:59 +0530

    Initialized String

--- Gitfiles/src/Gitpractice/samplegit.java
+++ Gitfiles/src/Gitpractice/samplegit.java
@@ -4,7 +4,7 @@ public class samplegit {
 	
 	public static void main(String args[])
 	{
-		String s = null;
+		String s = "Hello! World";
 		System.out.println("This is my first java program");
 		System.out.println("This is my first java program");
 		System.out.println("This is my first java program");
